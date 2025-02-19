7a2b85f80aa159e3553fb4e6a52ae4e57be88cd8
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-02-19 13:29:34 +0100

    Supporting different editing modes

--- App.js
+++ App.js
@@ -68,7 +68,14 @@ export default function App() {
         <>
             <StatusBar style="auto" />
             <NavigationContainer>
-                <Stack.Navigator>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: GlobalStyles.colors.primary500,
+                        },
+                        headerTintColor: "white",
+                    }}
+                >
                     <Stack.Screen
                         name="ExpensesOverview"
                         component={ExpensesOverview}
@@ -77,6 +84,7 @@ export default function App() {
                     <Stack.Screen
                         name="ManageExpense"
                         component={ManageExpense}
+                        options={{ presentation: "modal" }}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+    const editedExpensedId = route.params?.expenseId;
+    const isEditing = !!editedExpensedId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? "Edit Expense" : "Add Expense",
+        });
+    }, [navigation, isEditing]);
+
     return <Text>ManageExpense Screen</Text>;
 }
 
