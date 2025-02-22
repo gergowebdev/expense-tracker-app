4de30b64365432812eb1c506dbd0ad455d89b3c0
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-02-22 19:41:28 +0100

    Updating and Deleting backend data

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -5,7 +5,7 @@ import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
-import { storeExpense } from "../util/http";
+import { storeExpense, updateExpense, deleteExpense } from "../util/http";
 
 function ManageExpense({ route, navigation }) {
     const expensesCtx = useContext(ExpensesContext);
@@ -26,21 +26,23 @@ function ManageExpense({ route, navigation }) {
         });
     }, [navigation, isEditing]);
 
-    function deleteExpenseHandler() {
-        navigation.goBack();
+    async function deleteExpenseHandler() {
+        await deleteExpense(editedExpensedId);
         expensesCtx.deleteExpense(editedExpensedId);
+        navigation.goBack();
     }
 
     function cancelHandler() {
         navigation.goBack();
     }
 
-    function confirmHandler(expenseData) {
+    async function confirmHandler(expenseData) {
         if (isEditing) {
             expensesCtx.updateExpense(editedExpensedId, expenseData);
+            await updateExpense(editedExpensedId, expenseData);
         } else {
-            storeExpense(expenseData);
-            expensesCtx.addExpense(expenseData);
+            const id = await storeExpense(expenseData);
+            expensesCtx.addExpense({ ...expenseData, id: id });
         }
         navigation.goBack();
     }
